7171d78104dd330a7c65033919ec9e53c9466e3b
Jayakrishnan <jayakrishnan1012@gmail.com>
2022-06-26 13:11:13 +0530

    set up cache headers

--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -20,6 +20,13 @@ const GetUrl = async (req: NextApiRequest, res: NextApiResponse) => {
 
   if (!data) {
     res.statusCode = 404;
+
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Cache-Control",
+      "s-maxage=10000000000, stale-while-revalidate"
+    );
     res.send(JSON.stringify({ message: "Slug not found!" }));
     return;
   }
